599e108e87b5ac5b090e9c4fc1fc3a3bcd048f08
Toby Inkster <mail@tobyinkster.co.uk>
2012-02-03 00:51:42 +0000

    stash utils - in particular has_stash

--- lib/Object/Stash.pm
+++ lib/Object/Stash.pm
@@ -283,13 +283,15 @@ L<http://rt.cpan.org/Dist/Display.html?Queue=Object-Stash>.
 
 L<Object::New>, L<Object::ID>.
 
+L<Object::Stash::Util>.
+
 =head1 AUTHOR
 
 Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2011 by Toby Inkster.
+This software is copyright (c) 2011-2012 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
